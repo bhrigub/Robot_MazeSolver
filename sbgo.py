98f5095824daf55160e0e94ac079000407499e05
Frank Serna <sernaf1@umbc.edu>
2018-04-07 00:23:03 +0000

    Initial commit

---
+++ sbgo.py
@@ -0,0 +1,35 @@
+# Slaughterbot Go scripts and API
+
+# Turn the distance sensor some number of degrees 
+# (specified by an argument) right/left
+def turn_distance_sensor(int degrees):
+    pass
+
+# Turn wheel 1 or 2 forward or backward independently 
+# (this can be several functions, or a single function 
+# that takes arguments).
+def turn_wheels(int left, int right):
+    pass
+
+d.
+Control the wheels together to turn the robot 90 ° right/left
+
+e.
+Control the wheels together to t urn some number of degrees (specified by an argument) 
+right/left
+
+f.
+Turn the wheels in order to move the robot a specified distance forward or back (in cm).
+
+a.
+Get a single reading from the distance sensor.
+
+b.
+Get a continuous stream of readings from the distance sensor.
+
+c.
+Read the en coders’ position, in degrees. (See 
+github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py)
+
+d.
+Print the encoders’ positions in a continuous strea m.
