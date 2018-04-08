30f60115113da8fe854a91de625fbd06e65c6741
Frank Serna <sernaf1@umbc.edu>
2018-04-08 22:29:07 +0000

    distance: updating distance code to add continuous readings and threading

--- sbgo.py
+++ sbgo.py
@@ -1,48 +1,46 @@
 # Slaughterbot Go scripts and API
+# Authors: A, B, F
+# Attribution: code used from GoPiGo3 software found at:
+# https://github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py
 import sys
+import time
 
 sys.path.append('/home/pi/Dexter/GoPiGo3/Software/Python')
 
 import easygopigo3 as easy
+import multiprocessing as mp
 
 class SlaughterBot():
-    def __init__(self):
+    def __init__(self, distance_hz=1):
         self.gpg = easy.EasyGoPiGo3()
-	self.distance_sensor = self.gpg.init_distance_sensor()
+	    self.distance_sensor = self.gpg.init_distance_sensor()
+	    self.distance_hz = distance_hz
+        self.current_distance = mp.Value('i', 0)
 
-    #Get a single reading from the distance sensor.
+    # Get a single cm reading from the distance sensor.
+    # Attribution: code used from GoPiGo3 software found at:
+    # https://github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py
     def read_single_distance(self):
-        return self.distance_sensor.read_mm()
+        return self.distance_sensor.read()
+
+    # Attribution: code used from GoPiGo3 software found at:
+    # https://github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py
+    def read_continuous_distance(self):
+        delay = 1.0/self.distance_hz
+	    try:
+            while True:
+		        self.current_distance = self.read_single_distance()
+		        print("Current distance: {}".format(self.current_distance))
+                sleep(delay)
+        except KeyboardInterrupt:
+            self.gpg.reset_all()
 	
 
-# Turn the distance sensor some number of degrees 
-# (specified by an argument) right/left
-def turn_distance_sensor(degrees):
-    pass
-
-# Turn wheel 1 or 2 forward or backward independently 
-# (this can be several functions, or a single function 
-# that takes arguments).
-def turn_wheels(left, right):
-    pass
-
-#Control the wheels together to turn the robot 90 degrees right/left
-
-#Control the wheels together to t urn some number of degrees (specified by an argument) 
-#right/left
-
-#Turn the wheels in order to move the robot a specified distance forward or back (in cm).
-
-#Get a continuous stream of readings from the distance sensor.
-
-#Read the en coders position, in degrees. (See 
-#github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py)
-
-#Print the encoders positions in a continuous stream.
-
 def main():
-    # do stuff
-    pass
+    bot = SlaughterBot(4)
+    p = mp.Process(target=bot.read_continuous_distance)
+    p.start()
+    p.join()
 
 if __name__ == "__main__":
     main()
