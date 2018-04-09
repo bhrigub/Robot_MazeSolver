d6910cce72f91c2162ace101a3852de8e1b47248
rigsb <31597859+rigsb@users.noreply.github.com>
2018-04-08 22:47:00 -0400

    turn_degree addition

--- b_sbgo.py
+++ b_sbgo.py
@@ -8,19 +8,13 @@ import time
 sys.path.append('/home/pi/Dexter/GoPiGo3/Software/Python')
 
 import easygopigo3 as easy
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division  
 
 class SlaughterBot():
     def __init__(self):
         self.gpg = easy.EasyGoPiGo3()
         self.DEFAULT_SPEED = 300
-
-	     #self.distance_sensor = self.gpg.init_distance_sensor()
-        #self.forward_move=self.gpg.forward()
-        #self.backward_move=self.gpg.backward()
-        #self.left_move=self.gpg.left()
-        #self.right_move=self.gpg.right()
-        #self.stop_move=self.gpg.stop()
-        #self.drive_cm2=self.gpg.drive_cm()
         
     #Get a single reading from the distance sensor.
     def read_single_distance(self):
@@ -44,18 +38,44 @@ class SlaughterBot():
 #            self.drive_cm2(td_value, True)
 
     def turn_wheels (self,movement_direction):
+#        if (movement_direction == "forward"):
+#            self.gpg.forward()
+#        elif (movement_direction == "backward"):
+#            self.gpg.backward()
+#        elif (movement_direction == "left"):
+#            self.gpg.left()
+#        elif (movement_direction == "right"):
+#            self.gpg.right()
+#        else:
+#            self.gpg.stop()
         if (movement_direction == "forward"):
             self.gpg.forward()
         elif (movement_direction == "backward"):
             self.gpg.backward()
         elif (movement_direction == "left"):
-            self.gpg.left()
+            self.turnDegrees(-90,100)
         elif (movement_direction == "right"):
-            self.gpg.right()
+            self.turnDegrees(90,100)
         else:
-            self.gpg.stop()
+            self.turnDegrees(0,0)
 
-
+    def turnDegrees(self, degrees, speed):
+        # get the starting position of each motor
+        StartPositionLeft      = self.gpg.get_motor_encoder(self.gpg.MOTOR_LEFT)
+        StartPositionRight     = self.gpg.get_motor_encoder(self.gpg.MOTOR_RIGHT)
+        
+        # the distance in mm that each wheel needs to travel
+        WheelTravelDistance    = ((self.gpg.WHEEL_BASE_CIRCUMFERENCE * degrees) / 360)
+        
+        # the number of degrees each wheel needs to turn
+        WheelTurnDegrees       = ((WheelTravelDistance / self.gpg.WHEEL_CIRCUMFERENCE) * 360)
+        
+        # Limit the speed
+        self.gpg.set_motor_limits(self.gpg.MOTOR_LEFT + self.gpg.MOTOR_RIGHT, dps = speed)
+        
+        # Set each motor target
+        self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, (StartPositionLeft + WheelTurnDegrees))
+        self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, (StartPositionRight - WheelTurnDegrees))
 #Control the wheels together to turn the robot 90 degrees right/left
 
 #Control the wheels together to t urn some number of degrees (specified by an argument) 
@@ -72,19 +92,7 @@ class SlaughterBot():
 
 def main():
     gopigo=SlaughterBot()
-#    temp = 1
-#    gopigo.turn_wheels (temp)
-#    time.sleep (1)
-#    temp = 2
-#    gopigo.turn_wheels (temp)
-#    time.sleep (1)
-#    temp = 3
-#    gopigo.turn_wheels (temp)
-#    time.sleep (1)
-#    temp = 4
-#    gopigo.turn_wheels (temp)
-#    time.sleep (1)
-#                time.sleep (1)
+
     for i in range(0,12):
         if i < 5:
             if i % 2 == 1:
