039296f16a46f19a1a21fea4a5e8390899c51eb1
rigsb <31597859+rigsb@users.noreply.github.com>
2018-04-10 03:06:27 -0400

    updated
    updated

--- sbgo.py
+++ sbgo.py
@@ -22,24 +22,12 @@ class SlaughterBot():
     def read_single_distance(self):
         return self.distance_sensor.read_mm()
     
-    
 
-# Turn the distance sensor some number of degrees 
-# (specified by an argument) right/left
-    def turn_distance_sensor(degrees):
-        pass
-    
-    # Turn wheel 1 or 2 forward or backward independently 
-    # (this can be several functions, or a single function 
-    # that takes arguments).
-#    def wheelmove_time_distance(self,time_or_distance_parameter, td_value):
-#        if (time_or_distance_parameter == "time"):          
-#            self.forward()
-#            time.sleep(td_value)
-#        else:
-#            self.drive_cm2(td_value, True)
 
-# Robot movement in different directions
+#Function Objective: Move robot- Forward, Backward, Left, Right
+#Input Strings: forward, backward, left, right
+#Default Action: Stop
+
     def move_robot (self,movement_direction):
         if (movement_direction == "forward"):
             self.gpg.forward()
@@ -51,9 +39,22 @@ class SlaughterBot():
             self.turn_degrees(90,180)
         else:
             self.turn_degrees(0,0)
+            time.sleep(2)
+
+			
+
+#Function Objective: Move robot 'X' cm distance 
+#Input Strings: Distance value in cm
+#Default Action: N/A
+
+    def move_distance (self,distance_val):
+        self.gpg.drive_cm(distance_val,True)
 
 
-# Robot degree rotation using different combination of wheels
+#Function Objective: Turn robot by 'X' degree using both the wheels 
+#Input Strings: Rotation value in degree
+#Default Action: N/A
+
     def turn_degrees(self, degrees, speed):
         # get the starting position of each motor
         StartPositionLeft      = self.gpg.get_motor_encoder(self.gpg.MOTOR_LEFT)
@@ -71,44 +72,40 @@ class SlaughterBot():
         # Set each motor target
         self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, (StartPositionLeft + WheelTurnDegrees))
         self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, (StartPositionRight - WheelTurnDegrees))
-# Control wheel individually       
+        
+
+#Function Objective: Turn individual robot wheel 
+#Input Strings: Wheel selection string - leftf, leftb, rightf, rightb
+#Default Action: Stop
+
     def turn_wheel(self, wheel_turn):
         if (wheel_turn== "leftf"):
-            #self.gpg.left()
             self.gpg.set_motor_dps(self.gpg.MOTOR_RIGHT, 0)
             self.gpg.set_motor_dps(self.gpg.MOTOR_LEFT, self.gpg.get_speed())
-            #self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, 150)
-            #self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, 0)
         elif (wheel_turn== "leftb"):
-            #self.gpg.right()
             self.gpg.set_motor_dps(-self.gpg.MOTOR_LEFT, -self.gpg.get_speed())
             self.gpg.set_motor_dps(self.gpg.MOTOR_RIGHT, 0)
-            #self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, -150)
-            #self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, 0)
         elif (wheel_turn== "rightf"):
-            #self.gpg.right()
-            #self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, 0)
             self.gpg.set_motor_dps(self.gpg.MOTOR_LEFT, 0)
             self.gpg.set_motor_dps(self.gpg.MOTOR_RIGHT, self.gpg.get_speed())
-            #self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, 150)
         elif (wheel_turn== "rightb"):
-            #self.gpg.right()
-            #self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, 0)
             self.gpg.set_motor_dps(self.gpg.MOTOR_LEFT, 0)
             self.gpg.set_motor_dps(-self.gpg.MOTOR_RIGHT, -self.gpg.get_speed())
-            #self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, -150)
         else:
             self.turn_degrees(0,0)
+            time.sleep(2)
 
 
 def main():
     gopigo=SlaughterBot()
+
+    
     gopigo.move_robot ("forward")
-    time.sleep (1)
+    time.sleep (3)
     gopigo.move_robot ("left")
-    time.sleep (1)
+    time.sleep (3)
     gopigo.move_robot ("right")
-    time.sleep (2)
+    time.sleep (3)
     gopigo.move_robot ("boomchikiboom")
     gopigo.turn_wheel ("leftf")
     time.sleep (2)
@@ -119,26 +116,11 @@ def main():
     gopigo.turn_wheel ("rightb")
     time.sleep (2)
     gopigo.move_robot ("boomchikiboom")
+  
+
+    gopigo.move_distance(10)
     
 
-#    for i in range(0,12):
-#        if i < 5:
-#            if i % 2 == 1:
-#                gopigo.turn_wheels ("forward")
-#                time.sleep (1)
-#            else:
-#                gopigo.turn_wheels ("backward")
-#                time.sleep (1)
-#        elif i<10:
-#            if i % 2 == 1:
-#                gopigo.turn_wheels ("left")
-#                time.sleep (1)
-#            else:
-#                gopigo.turn_wheels ("right")
-#                time.sleep (1)
-#        else:
-#            gopigo.turn_wheels ("boomchikiboom")
-#    pass
 
 if __name__ == "__main__":
     main()
