e74a20b4bd4a84116124f431751ab588fa567415
rigsb <31597859+rigsb@users.noreply.github.com>
2018-04-11 01:50:10 -0400

    remove useless code

--- sbgo.py
+++ sbgo.py
@@ -1,21 +1,12 @@
 # Slaughterbot Go scripts and API
-<<<<<<< HEAD
 # Authors: A, B, F
 # Attribution: code used from GoPiGo3 software found at:
 # https://github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py
-import multiprocessing as mp
 
-import sys
-import time
-=======
-"""
-@author: Achin, Bhrigu, Frank
-References: https://github.com/DexterInd/GoPiGo3/blob/master/Software/Python/easygopigo3.py
-"""
 from __future__ import print_function # use python 3 syntax but make it compatible with python 2
 from __future__ import division 
->>>>>>> bBranch
 
+import multiprocessing as mp
 import sys
 import time
 sys.path.append('/home/pi/Dexter/GoPiGo3/Software/Python')
@@ -26,9 +17,8 @@ import easygopigo3 as easy
 class SlaughterBot():
     def __init__(self, distance_hz=0):
         self.gpg = easy.EasyGoPiGo3()
-<<<<<<< HEAD
-	self.distance_sensor = self.gpg.init_distance_sensor()
-	self.distance_hz = distance_hz
+        self.distance_sensor = self.gpg.init_distance_sensor()
+        self.distance_hz = distance_hz
         self.current_distance = mp.Value('i', 0)
 
     # Get a single cm reading from the distance sensor.
@@ -44,41 +34,6 @@ class SlaughterBot():
             delay = 0
         else:
             delay = 1.0/self.distance_hz
-	try:
-            while True:
-		self.current_distance = self.read_single_distance()
-		print("Current distance: {}".format(self.current_distance))
-                time.sleep(delay)
-        except KeyboardInterrupt:
-            self.gpg.reset_all()
-
-def main():
-    bot = SlaughterBot(4)
-    # let's start the continuous distance readings in another thread
-    p = mp.Process(target=bot.read_continuous_distance)
-    p.start()
-
-    # Simple algorithm to demonstrate proof of life
-    # Move forward until distance is 10cm or less
-    while bot.current_distance > 10:
-	bot.move_forward()
-
-    # Turn to the right 90 degrees
-    bot.turn_right(90)
-
-    # Aim distance sensor servo at wall
-    bot.turn_servo(180)
-
-    while bot.current_distance > 10:
-        bot.move_forward() 
-=======
-        self.DEFAULT_SPEED = 300
-        
-    #Get a single reading from the distance sensor.
-    def read_single_distance(self):
-        return self.distance_sensor.read_mm()
-    
-
 
 #Function Objective: Move robot- Forward, Backward, Left, Right
 #Input Strings: forward, backward, left, right
@@ -97,15 +52,12 @@ def main():
             self.turn_degrees(0,0)
             time.sleep(2)
 
-			
-
 #Function Objective: Move robot 'X' cm distance 
 #Input Strings: Distance value in cm
 #Default Action: N/A
 
     def move_distance (self,distance_val):
         self.gpg.drive_cm(distance_val,True)
-
 
 #Function Objective: Turn robot by 'X' degree using both the wheels 
 #Input Strings: Rotation value in degree
@@ -128,7 +80,6 @@ def main():
         # Set each motor target
         self.gpg.set_motor_position(self.gpg.MOTOR_LEFT, (StartPositionLeft + WheelTurnDegrees))
         self.gpg.set_motor_position(self.gpg.MOTOR_RIGHT, (StartPositionRight - WheelTurnDegrees))
-        
 
 #Function Objective: Turn individual robot wheel 
 #Input Strings: Wheel selection string - leftf, leftb, rightf, rightb
@@ -151,33 +102,6 @@ def main():
             self.turn_degrees(0,0)
             time.sleep(2)
 
-
-def main():
-    gopigo=SlaughterBot()
-
-    
-    gopigo.move_robot ("forward")
-    time.sleep (3)
-    gopigo.move_robot ("left")
-    time.sleep (3)
-    gopigo.move_robot ("right")
-    time.sleep (3)
-    gopigo.move_robot ("boomchikiboom")
-    gopigo.turn_wheel ("leftf")
-    time.sleep (2)
-    gopigo.turn_wheel ("leftb")
-    time.sleep (2)
-    gopigo.turn_wheel ("rightf")
-    time.sleep (2)
-    gopigo.turn_wheel ("rightb")
-    time.sleep (2)
-    gopigo.move_robot ("boomchikiboom")
-  
-
-    gopigo.move_distance(10)
-    
-
->>>>>>> bBranch
 
 if __name__ == "__main__":
     main()
